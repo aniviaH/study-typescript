b7dce61024ca9c05883ae9b0ac79a1a5165cbc98
liuhuan <aniviah@163.com>
2022-09-21 10:17:37 +0800

    feat(ReactHello.tsx): 修改react语法，react18中使用ReactDom.createRoot方法创建根实例

--- env-config/ts-webpack/src/ReactHello.tsx
+++ env-config/ts-webpack/src/ReactHello.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom/client'
+
+const id = document.getElementById('root')
 
 const App: (() => JSX.Element) = () => {
-  return <div>
+  return (<div>
     <h1>Hello React!</h1>
-  </div>
-}
+  </div>)
+} 
 
-console.log(1)
-ReactDom.render(<App />, document.getElementById('root'))
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(
+  <App />
+)
