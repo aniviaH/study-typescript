a8835b24a2e35cb3c3ff29ca9ec9d3cd2de419a0
liuhuan <aniviah@163.com>
2022-12-28 15:22:14 +0800

    feat: init daily-typescript

---
+++ daily-typescript/dist/index.js
@@ -0,0 +1,5 @@
+export function hello() {
+    const str = 'Hello World';
+    console.log('hello--------', str);
+}
+hello();
---
+++ daily-typescript/src/index.ts
@@ -0,0 +1,6 @@
+export function hello (): void {
+  const str: string = 'Hello World'
+  console.log('hello--------', str);
+}
+
+hello()
