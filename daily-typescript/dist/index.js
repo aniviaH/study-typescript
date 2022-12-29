d86f03717f9737e2f8c8507674d7e708c60ffd15
liuhuan <aniviah@163.com>
2022-12-29 19:17:02 +0800

    feat: daily-typescript

--- daily-typescript/dist/index.js
+++ daily-typescript/dist/index.js
@@ -1,5 +1,32 @@
-export function hello() {
-    const str = 'Hello World';
-    console.log('hello--------', str);
+"use strict";
+function hello() {
+    const str = "Hello World";
+    console.log("hello--------", str);
 }
 hello();
+/*
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = [0, 1, 2];
+// greeter(user)
+ */
+class Student {
+    fullName;
+    firstName;
+    lastName;
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+function greeter(person) {
+    const str = "Hello, " + person.firstName + " " + person.lastName;
+    console.log(str);
+    return str;
+}
+let user = new Student("Jane", "M.", "User");
+greeter(user);
+document.body.textContent = greeter(user);
--- daily-typescript/src/index.ts
+++ daily-typescript/src/index.ts
@@ -1,6 +1,59 @@
-export function hello (): void {
-  const str: string = 'Hello World'
-  console.log('hello--------', str);
+function hello(): void {
+  const str: string = "Hello World";
+  console.log("hello--------", str);
 }
 
-hello()
+hello();
+/*
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = [0, 1, 2];
+greeter(user)
+ */
+
+/* interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.textContent = greeter(user); */
+
+class Student {
+  fullName: string;
+  firstName: string;
+  lastName: string;
+  constructor(
+     firstName: string,
+     middleInitial: string,
+     lastName: string
+  ) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  const str = "Hello, " + person.firstName + " " + person
+  console.log(str);
+  return str
+}
+
+let user = new Student("Jane", "M.", "User");
+
+greeter(user);
+
+document.body.textContent = greeter(user);
