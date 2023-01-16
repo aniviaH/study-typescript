a1f1c9edc943e2a23e56636140768124d7f2b319
liuhuan <aniviah@163.com>
2023-01-16 10:37:37 +0800

    feat: typescriptlang.com--handlebook object-types

--- typescriptlang.com--handlebook/src/object-types/index.ts
+++ typescriptlang.com--handlebook/src/object-types/index.ts
@@ -646,3 +646,131 @@ obj5 = {a: 3}
 
 // let obj6: readonly string[] = ['a']
 
+
+/** 6. Tuple Types */
+
+// A tuple type is another sort of Array type that knows exactly how many elements it contains, and exactly which types it contains at specific positions.
+
+// type StringNumberPair = [string, number];
+
+// Here, StringNumberPair is a tuple type of string and number. Like ReadonlyArray, it has no representation at runtime, but is significant to TypeScript. 
+// To the type system, StringNumberPair describes arrays whose 0 index contains a string and whose 1 index contains a number.
+
+function doSomething6(pair: [string, number]) {
+  const a = pair[0];
+        // const a: string
+  const b = pair[1];
+        // const b: number
+  // const c = pair[2];
+                // Tuple type '[string, number]' of length '2' has no element at index '2'.
+  // ...
+}
+ 
+doSomething6(["hello", 1]);
+
+// We can also destructure tuples using JavaScript’s array destructuring.
+
+function doSomething7(stringHash: [string, number]) {
+  const [inputString, hash] = stringHash;
+  console.log(inputString);
+            // const inputString: string
+
+  console.log(hash);
+            // const hash: number
+}
+
+// Note: Tuple types are useful in heavily convention-based APIs, where each element’s meaning is “obvious”. 
+// This gives us flexibility in whatever we want to name our variables when we destructure them. 
+// In the above example, we were able to name elements 0 and 1 to whatever we wanted.
+// However, since not every user holds the same view of what’s obvious, it may be worth reconsidering whether using objects with descriptive property names may be better for your API.
+
+
+// Other than those length checks, simple tuple types like these are equivalent to types which are versions of Arrays that declare properties for specific indexes, and that declare length with a numeric literal type.
+
+interface StringNumberPair2 {
+  // specialized properties
+  length: 2;
+  0: string;
+  1: number;
+ 
+  // Other 'Array<string | number>' members...
+  slice(start?: number, end?: number): Array<string | number>;
+}
+
+function doSomething8(pair: StringNumberPair2) {
+  const a = pair[0];
+        // const a: string
+
+  const b = pair[1];
+        // const b: number
+}
+
+// Another thing you may be interested in is that tuples can have optional properties by writing out a question mark (? after an element’s type). 
+// Optional tuple elements can only come at the end, and also affect the type of length.
+
+type Either2dOr3d = [number, number, number?];
+ 
+function setCoordinate(coord: Either2dOr3d) {
+  const [x, y, z] = coord;
+              // const z: number | undefined
+  console.log(`Provided coordinates had ${coord.length} dimensions`);
+                                                // (property) length: 2 | 3
+}
+
+// Tuples can also have rest elements, which have to be an array/tuple type.
+
+type StringNumberBooleans = [string, number, ...boolean[]];
+type StringBooleansNumber = [string, ...boolean[], number];
+type BooleansStringNumber = [...boolean[], string, number];
+
+// StringNumberBooleans describes a tuple whose first two elements are string and number respectively, but which may have any number of booleans following.
+// StringBooleansNumber describes a tuple whose first element is string and then any number of booleans and ending with a number.
+// BooleansStringNumber describes a tuple whose starting elements are any number of booleans and ending with a string then a number.
+
+// A tuple with a rest element has no set “length” - it only has a set of well-known elements in different positions.
+
+const arr4: StringNumberBooleans = ["hello", 1];
+const arr5: StringNumberBooleans = ["beautiful", 2, true];
+const arr6: StringNumberBooleans = ["world", 3, true, false, true, false, true];
+
+// Why might optional and rest elements be useful? Well, it allows TypeScript to correspond tuples with parameter lists. 
+// Tuples types can be used in rest parameters and arguments, so that the following:
+
+function readButtonInput(...args: [string, number, ...boolean[]]) {
+  const [name, version, ...input] = args;
+  // ...
+}
+
+// is basically equivalent to:
+
+function readButtonInput2(name: string, version: number, ...input: boolean[]) {
+  // ...
+}
+
+// This is handy when you want to take a variable number of arguments with a rest parameter, and you need a minimum number of elements, but you don’t want to introduce intermediate variables.
+
+/** 6.1 readonly Tuple Types */
+
+// One final note about tuple types - tuples types have readonly variants, and can be specified by sticking a readonly modifier in front of them - just like with array shorthand syntax.
+
+function doSomething(pair: readonly [string, number]) {
+  // ...
+  // pair[0] = 'b' // Cannot assign to '0' because it is a read-only property.
+}
+
+// As you might expect, writing to any property of a readonly tuple isn’t allowed in TypeScript.
+
+// Tuples tend to be created and left un-modified in most code, so annotating types as readonly tuples when possible is a good default. 
+// This is also important given that array literals with const assertions will be inferred with readonly tuple types.
+
+let point = [3, 4] as const;
+ 
+function distanceFromOrigin([x, y]: [number, number]) {
+  return Math.sqrt(x ** 2 + y ** 2);
+}
+ 
+// distanceFromOrigin(point);
+// Argument of type 'readonly [3, 4]' is not assignable to parameter of type '[number, number]'.
+  // The type 'readonly [3, 4]' is 'readonly' and cannot be assigned to the mutable type '[number, number]'.
+
+// Here, distanceFromOrigin never modifies its elements, but expects a mutable tuple. Since point’s type was inferred as readonly [3, 4], it won’t be compatible with [number, number] since that type can’t guarantee point’s elements won’t be mutated.
