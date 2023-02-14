a4ad73a78c6f8a7304a6f542cb637cc4d386faf0
liuhuan <aniviah@163.com>
2023-02-14 11:05:53 +0800

    feat: typescriptlang.com--handlebook Reference Modules-Guidance for structure modules

--- typescriptlang.com--handlebook/src/reference/Modules/index.ts
+++ typescriptlang.com--handlebook/src/reference/Modules/index.ts
@@ -423,3 +423,244 @@ import { type TypeA, a} from "./api";
 // It can also be used as a global variable, but only inside of a script. (A script is a file with no imports or exports.)
 
 // mathLib.isPrime(2);
+
+
+/** Guidance for structuring modules */
+
+/** Export as close to top-level as possible */
+
+// Consumers of your module should have as little friction as possible when using things that you export. 
+// Adding too many levels of nesting tends to be cumbersome, so think carefully about how you want to structure things.
+
+// Exporting a namespace from your module is an example of adding too many layers of nesting. 
+// While namespaces sometime have their uses, they add an extra level of indirection when using modules. 
+// This can quickly become a pain point for users, and is usually unnecessary.
+
+// Static methods on an exported class have a similar problem - the class itself adds a layer of nesting. 
+// Unless it increases expressivity or intent in a clearly useful way, consider simply exporting a helper function.
+
+// If you’re only exporting a single class or function, use export default
+
+// Just as “exporting near the top-level” reduces friction on your module’s consumers, so does introducing a default export. 
+// If a module’s primary purpose is to house one specific export, then you should consider exporting it as a default export. 
+// This makes both importing and actually using the import a little easier. For example:
+
+// MyClass.ts
+// export default class SomeType {
+//   constructor() { ... }
+// }
+
+// MyFunc.ts
+// export default function getThing() {
+//   return "thing";
+// }
+
+// Consumer.ts
+// import t from "./MyClass";
+// import f from "./MyFunc";
+// let x = new t();
+// console.log(f());
+
+// This is optimal for consumers. They can name your type whatever they want (t in this case) and don’t have to do any excessive dotting to find your objects.
+
+// If you’re exporting multiple objects, put them all at top-level
+
+// MyThings.ts
+// export class SomeType {
+//   /* ... */
+// }
+// export function someFunc() {
+//   /* ... */
+// }
+
+// Conversely when importing:
+
+// Explicitly list imported names
+
+// import { SomeType, someFunc } from "./MyThings";
+// let x = new SomeType();
+// let y = someFunc();
+
+// Use the namespace import pattern if you’re importing a large number of things
+
+// MyLargeModule.ts
+// export class Dog { ... }
+// export class Cat { ... }
+// export class Tree { ... }
+// export class Flower { ... }
+
+// Consumer.ts
+// import * as myLargeModule from "./MyLargeModule.ts";
+// let x = new myLargeModule.Dog();
+
+
+/** Re-export to extend */
+
+// Often you will need to extend functionality on a module. 
+// A common JS pattern is to augment the original object with extensions, similar to how JQuery extensions work. 
+// As we’ve mentioned before, modules do not merge like global namespace objects would. 
+// The recommended solution is to not mutate the original object, but rather export a new entity that provides the new functionality.
+
+// Consider a simple calculator implementation defined in module Calculator.ts. 
+// The module also exports a helper function to test the calculator functionality by passing a list of input strings and writing the result at the end.
+
+// Calculator.ts
+// export class Calculator {
+//   private current = 0;
+//   private memory = 0;
+//   private operator: string;
+//   protected processDigit(digit: string, currentValue: number) {
+//     if (digit >= "0" && digit <= "9") {
+//       return currentValue * 10 + (digit.charCodeAt(0) - "0".charCodeAt(0));
+//     }
+//   }
+//   protected processOperator(operator: string) {
+//     if (["+", "-", "*", "/"].indexOf(operator) >= 0) {
+//       return operator;
+//     }
+//   }
+//   protected evaluateOperator(
+//     operator: string,
+//     left: number,
+//     right: number
+//   ): number {
+//     switch (this.operator) {
+//       case "+":
+//         return left + right;
+//       case "-":
+//         return left - right;
+//       case "*":
+//         return left * right;
+//       case "/":
+//         return left / right;
+//     }
+//   }
+//   private evaluate() {
+//     if (this.operator) {
+//       this.memory = this.evaluateOperator(
+//         this.operator,
+//         this.memory,
+//         this.current
+//       );
+//     } else {
+//       this.memory = this.current;
+//     }
+//     this.current = 0;
+//   }
+//   public handleChar(char: string) {
+//     if (char === "=") {
+//       this.evaluate();
+//       return;
+//     } else {
+//       let value = this.processDigit(char, this.current);
+//       if (value !== undefined) {
+//         this.current = value;
+//         return;
+//       } else {
+//         let value = this.processOperator(char);
+//         if (value !== undefined) {
+//           this.evaluate();
+//           this.operator = value;
+//           return;
+//         }
+//       }
+//     }
+//     throw new Error(`Unsupported input: '${char}'`);
+//   }
+//   public getResult() {
+//     return this.memory;
+//   }
+// }
+// export function test(c: Calculator, input: string) {
+//   for (let i = 0; i < input.length; i++) {
+//     c.handleChar(input[i]);
+//   }
+//   console.log(`result of '${input}' is '${c.getResult()}'`);
+// }
+
+// Here is a simple test for the calculator using the exposed test function.
+
+// TestCalculator.ts
+// import { Calculator, test } from "./Calculator";
+// let c = new Calculator();
+// test(c, "1+2*33/11="); // prints 9
+
+// Now to extend this to add support for input with numbers in bases other than 10, let’s create ProgrammerCalculator.ts
+
+// ProgrammerCalculator.ts
+// import { Calculator } from "./Calculator";
+// class ProgrammerCalculator extends Calculator {
+//   static digits = [
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//   ];
+//   constructor(public base: number) {
+//     super();
+//     const maxBase = ProgrammerCalculator.digits.length;
+//     if (base <= 0 || base > maxBase) {
+//       throw new Error(`base has to be within 0 to ${maxBase} inclusive.`);
+//     }
+//   }
+//   protected processDigit(digit: string, currentValue: number) {
+//     if (ProgrammerCalculator.digits.indexOf(digit) >= 0) {
+//       return (
+//         currentValue * this.base + ProgrammerCalculator.digits.indexOf(digit)
+//       );
+//     }
+//   }
+// }
+// Export the new extended calculator as Calculator
+// export { ProgrammerCalculator as Calculator };
+// Also, export the helper function
+// export { test } from "./Calculator";
+
+// The new module ProgrammerCalculator exports an API shape similar to that of the original Calculator module, but does not augment any objects in the original module. Here is a test for our ProgrammerCalculator class:
+
+// TestProgrammerCalculator.ts
+// import { Calculator, test } from "./ProgrammerCalculator";
+// let c = new Calculator(2);
+// test(c, "001+010="); // prints 3
+
+
+/** Do not use namespaces in modules */
+
+// When first moving to a module-based organization, a common tendency is to wrap exports in an additional layer of namespaces. 
+// Modules have their own scope, and only exported declarations are visible from outside the module. 
+// With this in mind, namespace provide very little, if any, value when working with modules.
+
+// On the organization front, namespaces are handy for grouping together logically-related objects and types in the global scope. 
+// For example, in C#, you’re going to find all the collection types in System.Collections. 
+// By organizing our types into hierarchical namespaces, we provide a good “discovery” experience for users of those types. 
+// Modules, on the other hand, are already present in a file system, necessarily. We have to resolve them by path and filename, so there’s a logical organization scheme for us to use. 
+// We can have a /collections/generic/ folder with a list module in it.
+
+// Namespaces are important to avoid naming collisions in the global scope.
+//  For example, you might have My.Application.Customer.AddForm and My.Application.Order.AddForm — two types with the same name, but a different namespace. 
+// This, however, is not an issue with modules. Within a module, there’s no plausible reason to have two objects with the same name. 
+// From the consumption side, the consumer of any given module gets to pick the name that they will use to refer to the module, 
+// so accidental naming conflicts are impossible.
+
+// For more discussion about modules and namespaces see <Namespaces and Modules>.
+
+
+/** Red Flags */
+
+// !!! All of the following are red flags for module structuring. 
+// Double-check that you’re not trying to namespace your external modules if any of these apply to your files:
+
+// A file whose only top-level declaration is [export namespace Foo { ... }] (remove Foo and move everything ‘up’ a level)
+// Multiple files that have the same [export namespace Foo {] at top-level (don’t think that these are going to combine into one Foo!)
