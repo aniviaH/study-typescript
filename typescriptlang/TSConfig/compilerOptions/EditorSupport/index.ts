98d32acee49348a7ee932b4c608404c0826045ae
1058039938@qq.com <aniviah@163.com>
2023-03-07 16:35:12 +0800

    feat: typescriptlang.com-TSConfig-CompilerOptions--Editor Support

---
+++ typescriptlang/TSConfig/compilerOptions/EditorSupport/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  borderr: 1px solid #666;
+`
