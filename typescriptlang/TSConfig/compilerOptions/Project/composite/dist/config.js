918bb07706fd53f488e54a9e90b7915e9a7ca4ef
1058039938@qq.com <aniviah@163.com>
2023-03-08 18:17:37 +0800

    feat: typescriptlang.com-TSConfig-CompilerOptions-Project

---
+++ typescriptlang/TSConfig/compilerOptions/Project/composite/dist/config.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.config = void 0;
+exports.config = {
+    name: 'name'
+};
---
+++ typescriptlang/TSConfig/compilerOptions/Project/composite/dist/src/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.hello = void 0;
+exports.hello = 'hi';
